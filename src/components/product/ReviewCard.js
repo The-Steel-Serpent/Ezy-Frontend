8505857e8dd90e7e5f36c325c30f186924a0056e
dungry853 <129316318+dungry853@users.noreply.github.com>
2024-09-25 00:07:30 +0700

    Product Detail

--- src/components/product/ReviewCard.js
+++ src/components/product/ReviewCard.js
@@ -1,7 +1,8 @@
 import { UserOutlined } from "@ant-design/icons";
-import { Avatar } from "antd";
+import { Avatar, Divider } from "antd";
 import React, { memo } from "react";
 import ReactStars from "react-rating-star-with-type";
+import { format } from "date-fns";
 const ReviewCard = ({ value }) => {
   const maskUsername = (username) => {
     if (!username) return "";
@@ -10,8 +11,12 @@ const ReviewCard = ({ value }) => {
     const maskedPart = "*".repeat(username.length - 2);
     return `${firstChar}${maskedPart}${lastChar}`;
   };
+
+  const formattedDate = value?.created_at
+    ? format(new Date(value?.created_at), "yyyy-MM-dd HH:mm")
+    : "";
   return (
-    <div className="flex flex-row items-center gap-3">
+    <div className="flex flex-row gap-3">
       <div className="w-fit">
         {value?.UserAccount?.avt_url != null ? (
           <img
@@ -24,8 +29,10 @@ const ReviewCard = ({ value }) => {
           </>
         )}
       </div>
-      <div className="flex flex-col">
-        <div className="">{maskUsername(value?.UserAccount?.username)}</div>
+      <div className="flex flex-col gap-1">
+        <div className="text-sm">
+          {maskUsername(value?.UserAccount?.username)}
+        </div>
         <ReactStars
           value={value?.rating}
           activeColor="#66cce6"
@@ -33,10 +40,18 @@ const ReviewCard = ({ value }) => {
           isEdit={false}
           size={13}
         />
+        <div className="flex flex-row items-center justify-center">
+          <div className="text-xs text-slate-500">{formattedDate}</div>
+          <Divider className="border-s-slate-400 h-4 top-0" type="vertical" />
+          <div className="text-xs text-slate-500">
+            Phân loại hàng:{" "}
+            {value?.ProductVarient?.ProductClassify?.product_classify_name +
+              " " +
+              value?.ProductVarient?.ProductSize?.product_size_name}
+          </div>
+        </div>
+        <div className="mt-2">{value?.review_content}</div>
       </div>
-      {/* <div className="flex flex-row">
-        <div className="">{value?.created_at}</div>
-      </div> */}
     </div>
   );
 };
