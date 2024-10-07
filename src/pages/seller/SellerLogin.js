896c55a84d17740fe32cf9ae287f7d1380e57e10
dungry853 <129316318+dungry853@users.noreply.github.com>
2024-10-07 15:38:06 +0700

    update seller login

--- src/pages/seller/SellerLogin.js
+++ src/pages/seller/SellerLogin.js
@@ -1,68 +1,69 @@
 import React, { useEffect, useState } from "react";
-import wallpaper from "../../assets/wallpaper-seller1.png"
+import wallpaper from "../../assets/wallpaper-seller1.png";
 import { Alert, Button, Divider, Input, Modal, message } from "antd";
 import { IoMdEye } from "react-icons/io";
 import { RiEyeCloseLine } from "react-icons/ri";
 import { FcGoogle } from "react-icons/fc";
-import { signInWithGoogle, signInWithEmailPassword, resetPassword } from "../../firebase/AuthenticationFirebase";
+import {
+  signInWithGoogle,
+  signInWithEmailPassword,
+  resetPassword,
+} from "../../firebase/AuthenticationFirebase";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { checkEmailFormat } from "../../helpers/formatEmail";
 
 const SellerLogin = () => {
-
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [success, setSuccess] = useState(null);
   const [error, setError] = useState(null);
   const [warning, setWarning] = useState(null);
   const [hidePassword, setHidePassword] = useState(false);
   const [isVisbleResetModal, setIsVisibleResetModal] = useState(false);
-  const [emailReset, setEmailReset] = useState('');
+  const [emailReset, setEmailReset] = useState("");
   const navigate = useNavigate();
 
   const handleHidePassword = (e) => {
     e.preventDefault();
-    setHidePassword(!hidePassword)
-  }
+    setHidePassword(!hidePassword);
+  };
 
   const showResetModal = () => {
     setIsVisibleResetModal(true);
-  }
+  };
 
   const handleEmailResetChange = (e) => {
     const emailReset = e.target.value;
     setEmailReset(emailReset);
     if (!emailReset) {
       setError("Email không được để trống");
-    }
-    else {
+    } else {
       setError(null);
     }
-  }
+  };
 
   const onSubmitReset = async (e) => {
     e.preventDefault();
     if (!emailReset) {
       setError("Email không được để trống");
       return;
-    }
-    else if (!checkEmailFormat(emailReset)) {
+    } else if (!checkEmailFormat(emailReset)) {
       setError("Email không hợp lệ");
       return;
     }
     try {
       await resetPassword(emailReset);
       setError(null);
-      setWarning('Vui lòng kiểm tra email để đặt lại mật khẩu');
+      setWarning("Vui lòng kiểm tra email để đặt lại mật khẩu");
     } catch (error) {
       setError(error.message);
     }
-  }
+  };
 
   const onCancelReset = () => {
     setIsVisibleResetModal(false);
-  }
+  };
 
   const checkEmail = async ({ email }) => {
     try {
@@ -70,39 +71,39 @@ const SellerLogin = () => {
       const res = await axios({
         method: "GET",
         url: URL,
-        withCredentials: true
+        withCredentials: true,
       });
-      if (res.status === 200)
-        return true;
-      else
-        return false;
+      if (res.status === 200) return true;
+      else return false;
     } catch (error) {
       console.log("Error check email:", error.message);
       return false;
     }
-  }
+  };
 
   const saveSeller = async ({ user_id, email }) => {
     try {
-      const res = await axios.post(`${process.env.REACT_APP_BACKEND_URL}/api/seller-register`, {
-        user_id,
-        email,
-      })
-      console.log('User created successfully:', res.data);
+      const res = await axios.post(
+        `${process.env.REACT_APP_BACKEND_URL}/api/seller-register`,
+        {
+          user_id,
+          email,
+        }
+      );
+      console.log("User created successfully:", res.data);
       return res.data;
     } catch (error) {
       console.log("Register error:", error.message);
       return error.message;
     }
-  }
-
+  };
 
   const handleGoogleSignIn = async (e) => {
     e.preventDefault();
     try {
       const user = await signInWithGoogle();
       // setSuccess('Successfully signed in with Google');
-      message.success('Đăng nhập thành công');
+      message.success("Đăng nhập thành công");
       const user_id = user.uid;
       const email = user.email;
       const check = await checkEmail({ email });
@@ -110,14 +111,13 @@ const SellerLogin = () => {
         const rs = await saveSeller({ user_id, email });
         if (rs) {
           console.log("Oke roi em oiw");
-        }
-        else {
+        } else {
           console.log("Loi roi em oi");
         }
-      }
-      else {
+      } else {
         console.log("Đã có tài khoản");
       }
+      localStorage.setItem("token", await user.getIdToken());
       navigate("/seller");
       setError(null);
     } catch (error) {
@@ -125,7 +125,7 @@ const SellerLogin = () => {
       setSuccess(null);
       console.log("Error:", error);
     }
-  }
+  };
 
   const handleSignInEmailPassword = async (e) => {
     e.preventDefault();
@@ -140,17 +140,15 @@ const SellerLogin = () => {
         const rs = await saveSeller({ user_id, email });
         if (rs) {
           console.log("Oke roi em oiw");
-        }
-        else {
+        } else {
           console.log("Loi roi em oi");
         }
-      }
-      else {
+      } else {
         console.log("Đã có tài khoản");
       }
       // setSuccess('Successfully email password');
       console.log("user:", user);
-      message.success('Đăng nhập thành công');
+      message.success("Đăng nhập thành công");
       navigate("/seller");
       setError(null);
     } catch (error) {
@@ -158,7 +156,7 @@ const SellerLogin = () => {
       setSuccess(null);
       console.log("Error:", error.message);
     }
-  }
+  };
 
   // Clear Alert
   useEffect(() => {
@@ -181,17 +179,17 @@ const SellerLogin = () => {
   return (
     <div className="bg-white w-full mt-1 shadow-inner flex justify-center gap-32">
       <div className="max-w-96 px-3 my-24 hidden lg:block">
-        <div className="text-primary text-3xl font-[490]">Bán hàng chuyên nghiệp</div>
-        <div className="text-slate-600 text-[21px] py-2">Quản lý shop của bạn một cách hiệu quả và Ezy hơn trên Ezy - Kênh Người bán</div>
-        <img
-          src={wallpaper}
-          width={500}
-          alt="wallpaper"
-        />
+        <div className="text-primary text-3xl font-[490]">
+          Bán hàng chuyên nghiệp
+        </div>
+        <div className="text-slate-600 text-[21px] py-2">
+          Quản lý shop của bạn một cách hiệu quả và Ezy hơn trên Ezy - Kênh
+          Người bán
+        </div>
+        <img src={wallpaper} width={500} alt="wallpaper" />
       </div>
       <div>
-        <form
-          className="lg:w-96 shadow-lg px-6 py-10 mb-10">
+        <form className="lg:w-96 shadow-lg px-6 py-10 mb-10">
           <h1 className="font-[450] text-xl mb-10">Đăng nhập</h1>
           <input
             type="text"
@@ -211,30 +209,37 @@ const SellerLogin = () => {
               onChange={(e) => setPassword(e.target.value)}
             />
             <button className="absolute right-3" onClick={handleHidePassword}>
-              {
-                hidePassword ? (
-                  <IoMdEye className="text-slate-500" size={25} />
-                ) : (
-                  <RiEyeCloseLine className="text-slate-500" size={25} />
-                )
-              }
+              {hidePassword ? (
+                <IoMdEye className="text-slate-500" size={25} />
+              ) : (
+                <RiEyeCloseLine className="text-slate-500" size={25} />
+              )}
             </button>
           </div>
           {/* success / error */}
           <div>
-            {success && <Alert message={success} type="success" showIcon className="mb-5" />}
-            {error && <Alert message={error} type="error" showIcon className="mb-5" />}
+            {success && (
+              <Alert
+                message={success}
+                type="success"
+                showIcon
+                className="mb-5"
+              />
+            )}
+            {error && (
+              <Alert message={error} type="error" showIcon className="mb-5" />
+            )}
           </div>
           <button
             onClick={handleSignInEmailPassword}
-            className="w-full bg-primary p-3 rounded text-white hover:bg-[#f3664a]">
+            className="w-full bg-primary p-3 rounded text-white hover:bg-[#f3664a]"
+          >
             Đăng nhập
           </button>
           <div className="w-full py-3 flex cursor-pointer">
-            <span
-              className="text-[15px] text-[#05a]"
-              onClick={showResetModal}
-            >Quên mật khẩu</span>
+            <span className="text-[15px] text-[#05a]" onClick={showResetModal}>
+              Quên mật khẩu
+            </span>
           </div>
           <Divider>
             <span className="text-slate-400 text-xs">HOẶC</span>
@@ -242,7 +247,8 @@ const SellerLogin = () => {
           <div className="w-full flex justify-between">
             <button
               onClick={handleGoogleSignIn}
-              className="border rounded p-2 w-[180px] hover:bg-slate-100 m-auto">
+              className="border rounded p-2 w-[180px] hover:bg-slate-100 m-auto"
+            >
               <span className="flex gap-1 justify-center items-center">
                 <FcGoogle size={23} /> Google
               </span>
@@ -250,7 +256,9 @@ const SellerLogin = () => {
           </div>
           <div className="text-sm mt-10 w-full flex justify-center">
             <span className="text-slate-400">Bạn mới biết đến Ezy?</span>
-            <a href="/seller/register" className="text-primary px-1">Đăng ký</a>
+            <a href="/seller/register" className="text-primary px-1">
+              Đăng ký
+            </a>
           </div>
         </form>
         <Modal
@@ -279,7 +287,7 @@ const SellerLogin = () => {
         </Modal>
       </div>
     </div>
-  )
+  );
 };
 
 export default SellerLogin;
