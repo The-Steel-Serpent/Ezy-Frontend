e2d98b5bd47d7b1118bf570a42f4107923e4213f
hntung <hongoctungii@gmail.com>
2024-11-02 23:31:47 +0700

    update

---
+++ src/components/admin/user/UserAccount.js
@@ -0,0 +1,74 @@
+import React, { useEffect, useState } from 'react';
+import { Table } from 'antd';
+import axios from 'axios';
+
+const UserAccount = () => {
+  const [userData, setUserData] = useState([]);
+
+  useEffect(() => {
+    axios.get(`${process.env.REACT_APP_BACKEND_URL}/api/all-user`)
+      .then(response => {
+        if (response.data.success) {
+          setUserData(response.data.data);
+        }
+      })
+      .catch(error => {
+        console.error("There was an error fetching the user data:", error);
+      });
+  }, []);
+
+  const columns = [
+    {
+      title: 'Username',
+      dataIndex: 'username',
+      key: 'username',
+    },
+    {
+      title: 'Họ và tên',
+      dataIndex: 'full_name',
+      key: 'full_name',
+    },
+    {
+      title: 'Email',
+      dataIndex: 'email',
+      key: 'email',
+    },
+    {
+      title: 'Số điện thoại',
+      dataIndex: 'phone_number',
+      key: 'phone_number',
+    },
+    {
+      title: 'Giới tính',
+      dataIndex: 'gender',
+      key: 'gender',
+    },
+    {
+      title: 'Ngày sinh',
+      dataIndex: 'dob',
+      key: 'dob',
+    },
+    {
+      title: 'Quyền tài khoản',
+      dataIndex: ['Role', 'role_name'],
+      key: 'role_name',
+    },
+    {
+      title: 'Đã xác thực',
+      dataIndex: 'isVerified',
+      key: 'isVerified',
+      render: (isVerified) => (isVerified ? 'Yes' : 'No'),
+    },
+  ];
+
+  return (
+    <Table
+      columns={columns}
+      dataSource={userData}
+      rowKey="user_id"
+      pagination={{ pageSize: 5 }}
+    />
+  );
+};
+
+export default UserAccount;
---
+++ src/pages/admin/user_management/AllRole.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Role from '../../../components/admin/user/Role'
+const AllRole = () => {
+    return (
+        <div className='bg-white rouned'>
+            <Role />
+        </div>
+    )
+}
+
+export default AllRole
