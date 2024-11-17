0de7362c969653e16f9189bef5048379f4592103
hntung <hongoctungii@gmail.com>
2024-11-17 23:43:51 +0700

    disable account

--- src/components/admin/user/ResolveViolationModal.js
+++ src/components/admin/user/ResolveViolationModal.js
@@ -1,8 +1,8 @@
 import React from "react";
-import { Modal, Button, Typography, Space } from "antd";
+import { Modal, Button, Typography, Space, notification } from "antd";
 import dayjs from "dayjs";
-
-const { Text, Title } = Typography;
+import { useSelector } from "react-redux";
+const { Text } = Typography;
 
 const ResolveViolationModal = ({
   visible,
@@ -10,7 +10,26 @@ const ResolveViolationModal = ({
   report,
   onApprove,
 }) => {
+  const adminId = useSelector((state) => state.user.user_id); 
   if (!report) return null;
+
+  const handleApprove = async () => {
+    try {
+      const response = await onApprove(adminId);
+      if (response.success) {
+        notification.success({
+          message: "Duyệt báo cáo thành công!",
+          description: `Quyết định xử phạt: ${response.result.penalty}`,
+        });
+        onClose();
+      }
+    } catch (error) {
+      notification.error({
+        message: "Lỗi khi duyệt báo cáo!",
+        description: error.message,
+      });
+    }
+  };
 
   return (
     <Modal
@@ -21,7 +40,7 @@ const ResolveViolationModal = ({
         <Button key="cancel" onClick={onClose}>
           Đóng
         </Button>,
-        <Button key="approve" type="primary" onClick={onApprove}>
+        <Button key="approve" type="primary" onClick={handleApprove}>
           Duyệt báo cáo
         </Button>,
       ]}
@@ -29,7 +48,8 @@ const ResolveViolationModal = ({
       <Space direction="vertical" size="middle">
         <Text strong>Loại vi phạm:</Text>
         <Text>{report.violation_type}</Text>
-
+        <Text strong>Id người gửi:</Text>
+        <Text>{report.sender_id}</Text>
         <Text strong>Ngày báo cáo:</Text>
         <Text>{dayjs(report.date_reported).format("DD/MM/YYYY HH:mm:ss")}</Text>
 
